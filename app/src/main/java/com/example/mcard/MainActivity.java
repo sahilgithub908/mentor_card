2536cd3fd1d1af7fc966d21412847017521619f2
098suraj <098suraj@gmail.com>
2021-11-14 22:21:38 +0530

    Studentid layout

--- app/src/main/java/com/example/mcard/MainActivity.java
+++ app/src/main/java/com/example/mcard/MainActivity.java
@@ -9,14 +9,27 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button btn1, btn2;
+Button btn1, btn2,btn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn1=findViewById(R.id.btn1);
         btn2=findViewById(R.id.btn2);
-
+        btn3=findViewById(R.id.btn3);
+        FragmentManager manager=getSupportFragmentManager();
+        FragmentTransaction transaction=manager.beginTransaction();
+        transaction.replace(R.id.Frame1, new Studentid());
+        transaction.commit();
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager=getSupportFragmentManager();
+                FragmentTransaction transaction=manager.beginTransaction();
+                transaction.replace(R.id.Frame1, new Studentid());
+                transaction.commit();
+            }
+        });
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
